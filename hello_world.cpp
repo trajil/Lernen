3dff5cc5ea7c2d0bdfb3d4bc11b70be1a431e3f8
trajil <148051488+trajil@users.noreply.github.com>
2023-10-30 14:39:09 +0100

    Add files via upload

---
+++ hello_world.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+
+    char Gruß[20] = "Hallo Welt!"; // Character mit 20 Stellen -1 für das NULL Zeichen am Ende
+
+
+	std::cout << Gruß << std::endl;
+
+
+
+}
---
+++ möhrenmobil.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    
+    int möhren_pro_km = 3;
+    int distanz = 34 ;
+    double Verbrauch = distanz * möhren_pro_km;
+
+
+    std::cout << Verbrauch <<std::endl;
+
+
+  
+}
