ae795ea42fffd8f577ffffda2b656f239589dce3
Trajil <trajil@web.de>
2023-11-02 16:01:02 +0100

    update

---
+++ rocket.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+/*
+1. the world is loaded with variables: altitude_start, altitude_goal, speed_start, speed_goal and fuel; constants: time(0.1 sec), gravity (depending on altitude and planet_mass);
+2. the player chooses one of the scenarios (launch_to_orbit_planet, landing_planet);
+    - launch_to_orbit_moon: altitude_start: 0, altitude_goal: 150, speed_start: ,speed_goal: ,  fuel: 2000, planet_mass: 1;
+    - launch_to_orbit_earth: altitude_start: 0, altitude_goal: 150, fuel: 2000, planet_mass: 6;
+    - landing_moon: altitude_start: 150, altitude_goal: 0, fuel: 200, planet_mass: 1;
+    - landing_earth: altitude_start: 150, altitude_goal: 0, fuel: 200, planet_mass: 6;
+3. 
+4.
+5.
+6.
+7.
+8.
+9.
+
+
+*/
