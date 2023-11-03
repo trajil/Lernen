db2fbb9cce3565ad8930cd8b5433b76b932161d8
trajil <trajil@web.de>
2023-11-03 07:36:35 +0100

    ds

--- rocket.cpp
+++ rocket.cpp
@@ -7,13 +7,12 @@
     - launch_to_orbit_earth: altitude_start: 0, altitude_goal: 150, speed_start: 0,speed_goal: 50, fuel: 2000, planet_mass: 6;
     - landing_moon: altitude_start: 150, altitude_goal: 0, speed_start: 10,speed_goal: <= 2, fuel: 200, planet_mass: 1;
     - landing_earth: altitude_start: 150, altitude_goal: 0, speed_start: 10,speed_goal: <= 2, fuel: 200, planet_mass: 6;
-3. 
-4.
-5.
-6.
-7.
-8.
-9.
+3. time starts ticking: every round is equals time_count;
+4. Each round has a loop: 
+    1) speed is subtracted from alt (alt from previous round - speed from previous round)
+    2) current gravity is calculated (gravity constant at new altitude)
+    3) 
+
 
 
 */
