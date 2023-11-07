2428acef0edad850b7cac6380215ba9fd5861c7d
Trajil <trajil@web.de>
2023-11-07 15:58:45 +0100

    new graphics added

--- rocket.cpp
+++ rocket.cpp
@@ -64,6 +64,19 @@ const double planet_mass_earth = 6; // work in progress...
 const double gravity_moon = 1.625; 
 const double gravity_earth = 9.81; 
 
+
+
+//texts and graphics
+std::string graphic_rocket_start = "       |\n      / \\\n     / _ \\\n    |.o '.|\n    |'._.'|\n    |     |\n  ,'|  |  |  |`.\n /  |  |  |  |  \\\n|,-'--|--'-.| t.r.\n";
+std::string welcome_text = "Hello, rocketman! Choose a scenario you wanna fly:\n1. Launch to orbit moon\n2. Launch to orbit earth\n3. Landing on moon\n4. Landing on earth\n";
+
+std::string graphic_space = "Space\n\n .              +   .                .   . .     .  .\n                   .                    .       .     *\n  .       *                        . . . .  .   .  + .\n            \"You Are Here\"            .   .  +  . . .\n.                 |             .  .   .    .    . .\n                  |           .     .     . +.    +  .\n                 \\|/            .       .   . .\n        . .       V          .    * . . .  .  +   .\n           +      .           .   .      +\n                            .       . +  .+. .\n  .                      .     . + .  . .     .      .\n           .      .    .     . .   . . .        ! /\n      *             .    . .  +    .  .       - O -\n          .     .    .  +   . .  *  .       . / |\n               . + .  .  .  .. +  .\n.      .  .  .  *   .  *  . +..  .            *\n .      .   . .   .   .   . .  +   .    .            +\n";
+std::string graphic_landed = "                     _  _     ____________.--.\n                  |\\|_|//_.-\"\" .'    \\   /|  |\n                  |.-\"\"\"-.|   /       \\_/ |  |\n                  \\  ||  /| __\\_____________ |\n                  _\\_||_/_| .-\"\"            \"\"-.  __\n                .' '.    \\//                    \".\/\n                ||   '. >()_                     |()<\n                ||__.-' |/\\ \\                    |/\\\n                   |   / \"|  \\__________________/.\"\"\n                  /   //  | / \\ \"-.__________/  /\\\n               ___|__/_|__|/___\\___\".______//__/__\\\n              /|\\     [____________] \\__/         |\\n             //\\ \\     |  |=====| |   /\\\\         |\\\\\n            // |\\ \\    |  |=====| |   | \\\\        | \\\\        ____...____....----\n          .//__| \\ \\   |  |=====| |   | |\\\\       |--\\---\"\"\"\"     .            ..\n_____....-//___|  \\_\\  |  |=====| |   |_|_\\\\      |___\\\\    .                 ...'\n .      .//-.__|_______|__|_____|_|_____[__\\\\_____|__.-\\      .     .    ...::\n        //        //        /          \\ `-_\\\\/         \\\\          .....:::\n  -... //     .  / /       /____________\\    \\\\       .  \\ \\     .            .\n      //   .. .-/_/-.                 .       \\\\        .-\\_\\-.                 .\n     / /      '-----'           .             \\ \\      '._____.'         .\n  .-/_/-.         .                          .-\\_\\-.                          ...\n '._____.'                            .     '._____.'                       .....\n        .                                                             ...... ..\n    .            .                  .                        .\n   ...                    .                      .                       .      .\n        ....     .                       .                    ....\n            ......           . ..                       ......'\n             .......             '...              ....\n                                   ''''''      .              .\n";
+std::string graphic_crashed = "     ________________________________         \n/                                \"-_          \n/      .  |  .                       \\          \n/      : \\ | / :                       \\         \n/        '-___-'                         \\      \n/_________________________________________ \\      \n     _______| |________________________--\"\"-L \n    /       F J                              \\ \n   /       F   J                              L\n  /      :'     ':                            F\n /        '-___-'                            / \n/_________________________________________--\n";
+std::string graphic_planet_moon = "                        .                          +\n           +                                                    .\n                                     ___       .\n     .                        _.--\"~~ __\"-.\n                           ,-\"     .-~  ~\"-\\              .\n              .          .^       /       ( )      .\n                    +   {_.---._ /         ~\n                        /    .  Y                            .\n                       /      \\_j                      +\n        .             Y     ( --l__\n                      |            \"-.                   .\n                      |      (___     \\\n              .       |        .)~-.__/             .           .\n                      l        _)\n     .                 \\      \"l\n         +              \\       \\\n                         \\       ^.\n             .            ^.       \"-.                    .\n                            \"-._      ~-.___,\n                      .         \"--.._____.^\n       .                                         .\n                            ->Moon<-\n";
+std::string graphic_planet_earth = "o               .        ___---___                    .                   \n       .              .--\\        --.     .     .         .                 \n                    ./.;_.\\     __/~ \\.                                     \n                   /;  / `-'  __\\    . \\                                    \n .        .       / ,--'     / .   .;   \\        |                         \n                 | .|       /       __   |      -O-       .                 \n                |__/    __ |  . ;   \\ | . |      |                         \n                |      /  \\\\_    . ;| \\___|                                \n   .    o       |      \\  .~\\\\___,--'     |           .                      \n                 |     | . ; ~~~~\\_    __|                                    \n    |             \\    \\   .  .  ; \\  /_/   .                                \n   -O-        .    \\   /         . |  ~/                  .                    \n    |    .          ~\\ \\   .      /  /~          o                        \n  .                   ~--___ ; ___--~                                       \n                 .          ---         .              -JT                   \n";
+
+
 // replay function
 bool askForReplay() 
 {
@@ -74,8 +87,9 @@ bool askForReplay()
 }
 
 // display: start of the game
-void displayScenarioParameters(double altitude_start, double altitude_goal, double speed_start, double speed_goal, double fuel, double planet_mass, std::string planet) 
+void displayScenarioParameters(double altitude_start, double altitude_goal, double speed_start, double speed_goal, double fuel, double planet_mass, std::string planet, std::string graphic_planet) 
 {
+    std::cout << graphic_planet;
     std::cout << "\nScenario Parameters:\n";
     std::cout << "Planet: " << planet << "\n";   
     //std::cout << "Starting Altitude: " << altitude_start << " meters\n";
@@ -190,14 +204,22 @@ void runScenario(double altitude_start, double altitude_goal, double speed_start
     std::cout << "" << std::endl;
 
     //check for game-ending conditions
-
     if (hp <= 0 )
     {
+        std::cout << graphic_crashed;
         std::cout << "" << std::endl;
         std::cout << "You Crashed!" << std::endl;
     }
     else if (launch == true && abs(speed) >= speed_goal || launch == false && hp > 0) 
     {
+        if (altitude > 0) 
+        {
+        std::cout << graphic_space;    
+        }
+        else
+        {
+        std::cout << graphic_landed; 
+        }
         std::cout << "" << std::endl;
         std::cout << "You won!" << std::endl;
     }
@@ -218,63 +240,56 @@ int main()
     {
     int scenario;
 
-    std::cout << "" << std::endl;
-    std::cout << "       |\n";
-    std::cout << "      / \\\n";
-    std::cout << "     / _ \\\n";
-    std::cout << "    |.o '.|\n";
-    std::cout << "    |'._.'|\n";
-    std::cout << "    |     |\n";
-    std::cout << "  ,'|  |  |  |`.\n";
-    std::cout << " /  |  |  |  |  \\\n";
-    std::cout << "|,-'--|--'-.| t.r.\n";
-    std::cout << "Hello, rocketman! Choose a scenario you wanna fly:\n1. Launch to orbit moon\n2. Launch to orbit earth\n3. Landing on moon\n4. Landing on earth\n";
-    std::cout << "" << std::endl;
+    std::cout << graphic_rocket_start << welcome_text;
     std::cin >> scenario;
 
     double altitude_start, altitude_goal, speed_start, speed_goal, fuel, planet_mass;
-    std::string planet;
+    std::string planet, graphic_planet;
     bool launch;
 
     switch (scenario) 
     {
         case 1:
             planet = "moon";
+            graphic_planet = graphic_planet_moon;
             altitude_start = 0;
             altitude_goal = 5000;
             speed_start = 0;
             speed_goal = 100;
-            fuel = 6800;
+            fuel = 4000;
             planet_mass = planet_mass_moon;
             launch = true;
             break;
         case 2:
             planet = "earth";
+            graphic_planet = graphic_planet_earth;
             altitude_start = 0;
             altitude_goal = 5000;
             speed_start = 0;
             speed_goal = 100;
-            fuel = 7500;
+            fuel = 4400;
             planet_mass = planet_mass_earth;
             launch = true;
             break;
         case 3:
             planet = "moon";
+            graphic_planet = graphic_planet_moon;
             altitude_start = 900;
             altitude_goal = 0;
-            speed_start = 10;
+            speed_start = 100;
             speed_goal = 2;
-            fuel = 600;
+            fuel = 250;
             planet_mass = planet_mass_moon;
             launch = false;
             break;
         case 4:
             planet = "earth";
+            graphic_planet = graphic_planet_earth;
             altitude_start = 900;
             altitude_goal = 0;
-            speed_start = 10;
+            speed_start = 100;
             speed_goal = 2;
-            fuel = 600;
+            fuel = 350;
             planet_mass = planet_mass_earth;
             launch = false;
             break;
@@ -283,7 +298,7 @@ int main()
             return 0;
     }
 
-    displayScenarioParameters(altitude_start, altitude_goal, speed_start, speed_goal, fuel, planet_mass, planet);
+    displayScenarioParameters(altitude_start, altitude_goal, speed_start, speed_goal, fuel, planet_mass, planet, graphic_planet);
     runScenario(altitude_start, altitude_goal, speed_start, speed_goal, fuel, planet_mass, launch);
     
     replay = askForReplay();
