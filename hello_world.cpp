e086f994009822c4718041e09f61fc95b372c8f7
Trajil <trajil@web.de>
2023-12-04 22:09:41 +0100

    new hello_world

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
-
+#include <string>
 int main()
 {
 
-    char Gruß[20] = "Hallo Welt!"; // Character mit 20 Stellen -1 für das NULL Zeichen am Ende
+    // char Gruß[20] = "Hallo Welt!"; // Character mit 20 Stellen -1 für das NULL Zeichen am Ende
+    //
+    // std::cout << Gruß << std::endl;
 
+    std::string hello("Hello World!\n");
 
-	std::cout << Gruß << std::endl;
+    std::cout << hello;
 
-
-
+    return 0;
 }
